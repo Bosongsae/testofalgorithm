7a2c54c82ee7ce33411d561b9c431947ea1c91ff
volavish <volavish@gmail.com>
2025-03-20 10:20:58 +0900

    gh-pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/testofalgorithm/', // GitHub Pages에서 사용될 경로 설정
-});
