5f355b9f2f44d5c516847775f15f440743616bff
volavish <volavish@gmail.com>
2025-03-20 16:44:25 +0900

    css update

--- src/App.tsx
+++ src/App.tsx
@@ -23,21 +23,21 @@ const selectLastHalfYear = contributions => {
 const App = () => {
   const usernames = ["GideokKim", "Bosongsae", "zer0ken", "Yoonsoyoung02", "trivialcoding6", "ChooSeongho", "codingorijazz","kairosial", "Ju-hong", "suyeon0702", "wnsdlfrns", "weg-9000", "serin826", "jiiiiiiiy", "YiHeeJu", "chaeengg", "Kristyn-Yoon", "yongsuk204", "Ryan-OH", "hongwon1031" ];
   const usernamessolved = ["potatooftaebaek", "covertnest", "zer0ken", "thdud4231", "trivialcoding6", "csh9895", "khuho777", "kairosial", "seesong", "dev_k", "jl92", "weg1456", "rin0742", "", "lehejo0330", "bchaeeun9", "kristyn00", "yongsuk204", "akrsoek0971", "hongwon1031"]
-  return (
+  return (    
     <div style={{ display: "flex" }}>
-      <h1>Algorithm Study</h1>
+      <h1 style={{width : "100%"}}>Algorithm Study</h1>
       <div id="github" style={{ width : "50%" }}>
       {usernames.map((username) => (
-        <div key={username} style={{ marginBottom: "30px", height : "100px"  }}>
-          <h2>{username}'s GitHub Contributions</h2>
+        <div key={username} style={{ marginBottom: "30px", height : "220px"  }}>
+          <h4>{username}'s GitHub Contributions</h4>
           <GitHubCalendar username={username} transformData={selectLastHalfYear} blockSize={10} />
         </div>
       ))}
       </div>
       <div id="solved" style={{ width : "50%"}}>
       {usernamessolved.map((username) => (
-        <div key={username} style={{ marginBottom: "30px", height : "100px" }}>
-          <h2>{username}'s Solved.ac Contributions</h2>
+        <div key={username} style={{ marginBottom: "30px", height : "220px" }}>
+          <h4>{username}'s Solved.ac Streak</h4>
           <img src={`http://mazandi.herokuapp.com/api?handle=${username}&theme=warm`} />
         </div>))}
       </div>
