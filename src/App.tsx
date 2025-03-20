85b4e312499523aa4b3e08fc72753ec7cafcc5bc
volavish <volavish@gmail.com>
2025-03-20 09:52:27 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import GitHubCalendar from 'react-github-calendar';
 
 const App = () => {
+  const usernames = ["Bosongsae", "userA", "userB", "userC"];
   return (
     <div>
-      <h1>My GitHub Contributions</h1>
-      <GitHubCalendar username="Bosongsae" />
-    </div>
+      <h1>GitHub Contributions</h1>
+      {usernames.map((username) => (
+        <div key={username} style={{ marginBottom: "30px" }}>
+          <h2>{username}'s Contributions</h2>
+          <GitHubCalendar username={username} blockSize={10} />
+        </div>
+      ))}
+    </div>   
   );
 };
 
