8f9bfc05c9dcd3f02261724a1242270a28636848
volavish <volavish@gmail.com>
2025-03-20 20:00:27 +0900

    months update

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import GitHubCalendar from 'react-github-calendar';
 
 
-const selectLastHalfYear = contributions => {
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
-  const shownMonths = 6;
+const selectLastHalfYear = (contributions) => {
+  const now = new Date();
+  const sixMonthsAgo = new Date();
+  sixMonthsAgo.setMonth(now.getMonth() - 5); // 현재 달 포함해서 6개월치
 
-  return contributions.filter(activity => {
+  return contributions.filter((activity) => {
     const date = new Date(activity.date);
-    const monthOfDay = date.getMonth();
-
-    return (
-      date.getFullYear() === currentYear &&
-      monthOfDay > currentMonth - shownMonths &&
-      monthOfDay <= currentMonth
-    );
+    return date >= sixMonthsAgo && date <= now;
   });
 };
 
